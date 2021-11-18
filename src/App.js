bcb30ca6e66e93960dc3f3196cf51b8969fcc90d
motohirotakagi <tkg.8.mthr@gmail.com>
2021-11-18 12:44:37 +0900

    useEffectの使い方

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React, { useState } from 'react'
+import React, {useEffect, useState } from 'react'
 
 const App = props => {
   const [state, setState] = useState(props)
   const [name, setName] = useState(props.name)
   const [price, setPrice] = useState(props.price)
+
+  const { name, price } = state
+
+  useEffect(() => {
+    console.log('レンダリングの後で毎回実行される')
+  })
+
+  useEffect(() => {
+    console.log('最初の一回のみ実行される')
+  }, [])
+
+  useEffect(() => {
+    console.log('nameが変更された時のみ実行される（コールバック）')
+  }, [name])
 
   return (
     <>
