b1a83b9bb076dffeec58b9b1ece43d05c3028d1e
motohirotakagi <tkg.8.mthr@gmail.com>
2021-11-21 21:49:16 +0900

    reducerを使った状態遷移

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,43 @@
-import { getTableBodyUtilityClass } from '@mui/material';
+// import { getTableBodyUtilityClass } from '@mui/material';
+import React, { useReducer, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import reducer from '../reducers'
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, [])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const addEvent = e => {
+    e.preventDefault()
+
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body
+    })
+
+    setTitle('')
+    setBody('')
+  }
+
+  console.log({state})  
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
       <form>
         <div className="form-group">
           <label htmlFor="formEventTitle">タイトル</label>
-          <input className="form-control" id="formEventTitle" />
+          <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)}/>
         </div>
         
         <div className="form-group">
           <label htmlFor="formEventBody">ボディ</label>
-          <textarea className="form-control" id="formEventBody" />
+          <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)}/>
         </div>
 
-        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
         <button className="btn btn-danger">全てのイベントを削除する</button>
       </form>
       <h4>イベント一覧</h4>
@@ -30,7 +51,16 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-
+          {
+            state.map(event => (
+              <tr>
+                <td>{event.id}</td>
+                <td>{event.title}</td>
+                <td>{event.body}</td>
+                <td><button type="button" className="btn btn-danger">削除</button></td>
+              </tr>
+            ))
+          }
         </tbody>
 
       </table>
